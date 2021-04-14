315a7f98940820503d79ab14c9fb21cf989a3a99
FengF <fengfeng.ljf@alibaba-inc.com>
2021-04-14 22:22:37 +0800

    DesignPattern-factory-simple-refactor.

--- DesignPattern/src/factory/simpleFactory/RuleConfigSource.java
+++ DesignPattern/src/factory/simpleFactory/RuleConfigSource.java
@@ -3,6 +3,19 @@ package factory.simpleFactory;
 public class RuleConfigSource {
   public RuleConfig load(String ruleConfigFilePath){
     String ruleConfigFileExtension = getFileExtension(ruleConfigFilePath);
+    IRuleConfigParse parser = createParser(ruleConfigFileExtension);
+    if(parser == null) {
+      throw new InvalidRuleConfigException(
+        "Rule config file format is not supported." + ruleConfigFileExtension);
+    }
+
+    String configText = "";
+    //从ruleConfigFilePath文件中读取配置文本到configText中
+    RuleConfig ruleConfig = parser.parse(configText);
+    return ruleConfig;
+  }
+
+  private IRuleConfigParse createParser(String ruleConfigFileExtension) {
     IRuleConfigParse parser = null;
     if("json".equalsIgnoreCase(ruleConfigFileExtension)){
       parser = new JsonRuleConfigParser();
@@ -10,18 +23,10 @@ public class RuleConfigSource {
       parser = new XmlRuleConfigParser();
     } else if("yaml".equalsIgnoreCase(ruleConfigFileExtension)){
       parser = new yamlRuleConfigParser();
-    } else if("properties".equalsIgnoreCase(ruleConfigFileExtension)){
+    } else if ("properties".equalsIgnoreCase(ruleConfigFileExtension)){
       parser = new PropertiesRuleConfigParser();
-    } else {
-      throw new InvalidRuleConfigException(
-        "Rule config file format is not supported: " + ruleConfigFilePath
-      );
     }
-
-    String configText = "";
-    //从ruleConfigFilePath文件中读取配置文本到configText中
-    RuleConfig ruleConfig = parser.parse(configText);
-    return ruleConfig;
+    return parser;
   }
 
   private String getFileExtension(String filePath){
